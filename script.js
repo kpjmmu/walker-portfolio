305245629578eb1958c0e2b0ed8bdf2c75b6d0d8
kpjmmu <jrwalkerps@gmail.com>
2022-09-21 16:44:58 -0400

    General Update
    Fixing animations, header structure

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const arrow = document.querySelector(".arrow-container")
+const arrow = document.querySelector(".arrow-container");
+const navOpen = document.querySelector(".open-menu");
+const navClose = document.querySelector(".close-menu")
+const navList = document.querySelector(".nav-list");
 
+// Timer for arrow fade-in
 setTimeout(() => {
-    arrow.classList.add("fadeInLeft", "animate");
+    arrow.classList.add("fadeInDown", "animate");
     arrow.classList.remove("invisible");
 }, "4000");
+
+navOpen.addEventListener("click", () => {
+    navList.classList.remove("hidden");
+})
+
+navClose.addEventListener("click", () => {
+    navList.classList.add("hidden");
+})
