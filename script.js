49107b3a59bb49e0fedda6d17fd4b9c4ed80ab5a
kpjmmu <jrwalkerps@gmail.com>
2022-09-21 15:15:50 -0400

    Update
    -Added animation to main-title, arrow
    -Added js for timeout of arrow animation

---
+++ script.js
@@ -0,0 +1,6 @@
+const arrow = document.querySelector(".arrow-container")
+
+setTimeout(() => {
+    arrow.classList.add("fadeInLeft", "animate");
+    arrow.classList.remove("invisible");
+}, "4000");
